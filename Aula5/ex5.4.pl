36234d701ce6f4f919eda2afcbe879188b7e7e80
Rafael Freitas da Silva <rfl.fsilva@gmail.com>
2014-04-15 20:16:11 -0300

    Corrected ex5.4

--- Aula5/ex5.4.pl
+++ Aula5/ex5.4.pl
@@ -4,12 +4,11 @@
 %% Exemplo de query:
 %% ?- fat(10, X)
 
-fat(0, 1)
-fat(1, 1)
+fat(0, 1).
 fat(N, NF) :-
- 	A is N - 1, B is N - 2,
- 	fat(A, AF), fat(B, BF),	 	
-  	NF is AF*BF.
+ 	K is N - 1,
+ 	fat(K, KF),	 	
+  	NF is N*KF.
  
 
   
